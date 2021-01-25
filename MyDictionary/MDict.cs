9ad719d82147d8abbdb7e6b14805d9ec647fa5fd
cigdemyildiz <cigdemyildiz1463@users.noreply.github.com>
2021-01-25 22:46:45 +0300

    Add project files.

---
+++ MyDictionary/MDict.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MDict <T,Y>
+    {
+        T[] key;
+        T[] tempKey;
+        Y[] value;
+        Y[] tempValue;
+        public MDict()
+        {
+
+            key = new T[0];
+            value = new Y[0];
+        }
+        public void Add(T item1, Y item2)
+        {
+            tempKey = key;
+            tempValue = value;
+
+            key = new T[key.Length + 1];
+            value = new Y[value.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+                value[i] = tempValue[i];
+            }
+            key[key.Length - 1] = item1;
+            value[value.Length - 1] = item2;
+
+        }
+        
+        public void Print()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine(key[i]+ " " + value[i]);
+            }
+        }
+
+        public int Count
+        {
+            get { return key.Length;  }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            MDict<int, string> Ogrenciler = new MDict<int, string>();
+            Ogrenciler.Add(1, "Çiğdem");
+            Ogrenciler.Add(2, "Yıldız");
+
+            Console.WriteLine("---Öğrencilerin Listelenmesi---");
+            Ogrenciler.Print();
+
+            Console.WriteLine("---Öğrencilerin Sayısı---");
+            Console.WriteLine(Ogrenciler.Count);
+        }
+    }
+}
